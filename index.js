b44dc5027df0d7d4b75bb6a2bea693588623996c
Atlas <jack@bentsen.us>
2021-07-21 14:55:39 -0700

    adding note request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,10 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json('./db/db.json');
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
