3ad72544874fd47af44ac4a9fc785a72e589726b
Atlas <jack@bentsen.us>
2021-07-21 22:32:54 -0700

    got post method working

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    console.log(`${req.method} Request recieved to main page...`);
 });
 
 app.get('/notes', (req, res) => {
+    console.log(`${req.method} recieved for Notes Page`);
     res.sendFile(path.join(__dirname, 'public/notes.html'));
-})
+});
 
 app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved for notes`);
-    res.json(JSON.parse(fs.readFile('./db/db.json')));
-})
+    console.info(`${req.method} request to API recieved for notes`);
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        res.json(JSON.parse(data));
+    });
+});
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved to add a note`);
+    const newNote = req.body;
+    newNote.id = uniqid();
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        let currentNotes = JSON.parse(data)
+        currentNotes.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(currentNotes), err => console.log(err));
+    })
+    res.send('Recieved Note Add call..');
+});
 
+app.delete('/api/notes', (req, res) => {
     
-
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-    console.info(`${req.method} request recieved for notes`);
-})
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
